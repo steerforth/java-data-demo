257215ed1a0e459969360786027da786ccc67378
steer <565355716@qq,com>
2019-09-05 20:02:06 +0800

    init

---
+++ src/main/java/com/steer/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.steer.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Object hello(){
+        return "nihao";
+    }
+}
